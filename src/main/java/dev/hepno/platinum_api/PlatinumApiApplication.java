9d5ab8666baa235919b8b14a7ca170489aeac6f9
Lily Lindstrand <lily@hepno.dev>
2025-02-22 21:18:31 +0100

    init

---
+++ src/main/java/dev/hepno/platinum_api/PlatinumApiApplication.java
@@ -0,0 +1,13 @@
+package dev.hepno.platinum_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatinumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatinumApiApplication.class, args);
+	}
+
+}
